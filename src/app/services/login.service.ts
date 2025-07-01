2e6779beb51808e71b2a2e794c07049e80e8a551
Hernan590 <herjosuee@hotmail.com>
2025-06-30 19:09:24 -0500

    Agregando api

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../enviroments/enviroments';
+import { environment } from '../../enviroments/enviroments.prod';
 import { Router } from '@angular/router';
 
 @Injectable({
